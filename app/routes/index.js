025b130a3eedec332f405b7f6729ffd3c9807ad4
Carl Egbert and Joel Delight <carl+joel@>
2016-08-16 11:35:13 -0700

    add a carousel

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('story');
+    return Ember.RSVP.hash({
+      carousel: this.store.query('story', {orderBy: 'time', limitToLast: 3}),
+      stories: this.store.findAll('story')
+  });
   }
 });
